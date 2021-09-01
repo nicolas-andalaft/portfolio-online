3314c3b72d60120b3053ca8df71d31bae52ed251
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-09-01 00:00:29 -0300

    Modo dark

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -88,3 +88,10 @@ function changeNavbarOnScroll() {
 		}
 	});
 }
+
+function changeTheme() {
+	const currentTheme = document.documentElement.getAttribute("data-theme");
+	if (currentTheme === "light")
+		document.documentElement.setAttribute("data-theme", "dark");
+	else document.documentElement.setAttribute("data-theme", "light");
+}
